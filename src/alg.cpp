56875360807787bf686d9b7d4b362ab6734e3a41
Mirghit <yuandru25@gmail.com>
2026-04-18 17:39:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,52 +16,44 @@ bool isOperator(char s) {
 std::string infx2pstfx(const std::string& inf) {
   TStack<char, 100> stack;
   std::string result;
-
   for (size_t i = 0; i < inf.size(); i++) {
     char c = inf[i];
-
     if (c == ' ') continue;
-
     if (c >= '0' && c <= '9') {
       while ((i < inf.size()) && (inf[i] >= '0' && inf[i] <= '9')) {
-        result += inf[i++];
+        result += inf[i];
+        i++;
       }
-      result += " ";
+      result += ' ';
       i--;
-    }
-    else if (c == '(') {
+    } else if (c == '(') {
       stack.push(c);
-    }
-    else if (c == ')') {
+    } else if (c == ')') {
       while (!stack.isEmpty() && stack.top() != '(') {
         result += stack.pop();
-        result += " ";
+        result += ' ';
       }
       stack.pop();
-    }
-    else if (isOperator(c)) {
+    } else if (isOperator(c)) {
       while (!stack.isEmpty() && priorityCheck(stack.top()) >= priorityCheck(c)) {
         result += stack.pop();
-        result += " ";
+        result += ' ';
       }
       stack.push(c);
     }
   }
   while (!stack.isEmpty()) {
     result += stack.pop();
-    result += " ";
+    result += ' ';
   }
   return result;
 }
 
 int eval(const std::string& pref) {
   TStack<int, 100> stack;
-
   for (size_t i = 0; i < pref.size(); i++) {
     char c = pref[i];
-
     if (c == ' ') continue;
-
     if (c >= '0' && c <= '9') {
       int number = 0;
       while ((i < pref.size()) && (pref[i] >= '0' && pref[i] <= '9')) {
@@ -70,8 +62,7 @@ int eval(const std::string& pref) {
       }
       stack.push(number);
       i--;
-    }
-    else {
+    } else {
       int last = stack.pop();
       int preLast = stack.pop();
       if (c == '+') stack.push(preLast + last);
